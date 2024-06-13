d43e39b6c395fe572c004d7edf0d9ee0ca072587
Alan Jacobs <ahjacobs@inf.ufsm.br>
2024-06-13 16:08:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+LSTM to the end is me coding a mini, mini, micro chatbot to ensure that im understanding what im studying
---
+++ simpleLSTM.py
@@ -0,0 +1,25 @@
+import keras
+
+from keras.layers import LSTM
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+
+input = []
+
+with open("frases_manuscritas.txt", 'r') as file:
+    input = file.read()
+    input = input.split("\n")
+
+tokenizer = Tokenizer()
+# pega o input e da um indice para cada palavra de acordo com a frequencia que ela aparece
+tokenizer.fit_on_texts(input)
+# troca as palavras pelo seu indice
+sequences = tokenizer.texts_to_sequences(input)
+# deixa todas as sequencias com o mesmo tamanho adicionando 0s
+padded_sequences = pad_sequences(sequences)
+
+print(padded_sequences)
+
+model = Sequential()
+model.add
